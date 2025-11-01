10008432925fd5cae8eccefd86b43cd4b71c69c1
Ömer Faruk Sarı <omerfaruksari0077@gmail.com>
2025-11-01 19:02:02 +0300

    Setup

---
+++ Assets/PewPew/Bullet/BulletProjectile.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody;
+
+    private void Awake()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        float speed = 45f;
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BulletTarget>() != null)
+        {
+            //hit target
+        }
+        else
+        {
+            //hit something else
+        }
+        Destroy(gameObject);
+    }
+}
