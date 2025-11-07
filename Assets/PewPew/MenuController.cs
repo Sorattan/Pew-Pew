2747d9ca6255a6275880b3c99bacacfd795cdcab
farukomerg <guleromerfaruk10@gmail.com>
2025-11-07 12:26:42 +0300

    Login Page Second Commit

--- Assets/PewPew/MenuController.cs
+++ Assets/PewPew/MenuController.cs
@@ -1,17 +1,60 @@
+using UnityEngine.UI;
+using UnityEditor.SettingsManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void Cikis(){
+    public GameObject K;
+    public GameObject Panel;
+    public GameObject Smenu;
+    public Slider Sldr;
+
+    public float SensK;
+    
+    public void Cikis()
+    {
         Application.Quit();
     }
     public void Başla()
     {
         SceneManager.LoadScene(1);
     }
-    public void MainMenu(){
+    public void MainMenu()
+    {
         SceneManager.LoadScene(0);
     }
+
+    void Setting(GameObject kapanacakolan, GameObject acilacakolan, GameObject panel)
+    {
+        kapanacakolan.SetActive(false);
+        acilacakolan.SetActive(true);
+        panel.SetActive(false);
+        Smenu.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        Setting(K, Smenu, Panel);
+    }
+
+    private void Update()
+    {
+        SensK = Sldr.value;
+
+        PlayerPrefs.SetFloat("Sens", SensK);
+
+        print(PlayerPrefs.GetFloat("Sens"));
+    }
+
+    public void GeriD()
+    {
+        Panel.SetActive(true);
+        Smenu.SetActive(false);
+        K.SetActive(true);
+    }
+
+
 }
+
 
